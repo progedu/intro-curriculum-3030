18cadeeacbc3548baeecbe8b355f8cfa2a4389a9
yusuke_nakajima <www.yyworld@gmail.com>
2017-08-21 14:28:29 +0900

    コード整形

--- test.js
+++ test.js
@@ -4,15 +4,15 @@ const assert = require('assert');
 
 // jade のテンプレートにおける XSS 脆弱性のテスト
 const html = jade.renderFile('./views/posts.jade', {
-  posts: [{
-    id: 1,
-    content: '<script>alert(\'test\');</script>',
-    postedBy: 'guest1',
-    trackingCookie: '2639292283224063_ddcc625203464a9e10af58fc3eb92eed7df4b9b5',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }],
-  user: 'guest1'
+    posts: [{
+        id: 1,
+        content: '<script>alert(\'test\');</script>',
+        postedBy: 'guest1',
+        trackingCookie: '2639292283224063_ddcc625203464a9e10af58fc3eb92eed7df4b9b5',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }],
+    user: 'guest1'
 });
 
 // スクリプトタグがエスケープされて含まれていることをチェック

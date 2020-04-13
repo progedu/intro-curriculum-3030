673601da83984ed92fb89c1d68872cedfbec362e
gnotes3sound <isojin41@yahoo.co.jp>
2020-04-13 20:49:29 +0900

    ハッシュ値の非表示化

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ const html = pug.renderFile('./views/posts.pug', {
     id: 1,
     content: '<script>alert(\'test\');</script>',
     postedBy: 'guest1',
-    trackingCookie: '2639292283224063_ddcc625203464a9e10af58fc3eb92eed7df4b9b5',
+    trackingCookie: '2104493986441693_133d648e476e9ce3d7f7b812916ba1ddf76bfe7b',
     createdAt: new Date(),
     updatedAt: new Date()
   }],
@@ -17,5 +17,7 @@ const html = pug.renderFile('./views/posts.pug', {
 
 // スクリプトタグがエスケープされて含まれていることをチェック
 assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+assert(html.includes('2104493986441693'));
+assert(!html.includes('133d648e476e9ce3d7f7b812916ba1ddf76bfe7b'));
 console.log('テストが正常に完了しました');
 
